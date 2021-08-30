97f835f3778a876827d49627b9bed1a1007c3619
Alvaro Torres Cogollo <atorrescogollo@gmail.com>
2021-08-30 18:59:52 +0200

    First version

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
 # k8s-volume-injector
+Mount volumes to workloads using MutatingWebhookConfiguration. Based on: https://github.com/tumblr/k8s-sidecar-injector
+
+
+## Requirements
+You must have cert-manager installed.
+
+## How to use it
+```bash
+# Install tumblr/k8s-sidecar-injector & webhook
+export VERSION="main"
+curl -s https://raw.githubusercontent.com/atorrescogollo/k8s-volume-injector/${VERSION}/deploy/install.sh | bash
+```
+
+```bash
+# Check installation
+$ kubectl -n k8s-volume-injector get mutatingwebhookconfigurations,svc,deploy
+NAME                                                                            WEBHOOKS   AGE
+mutatingwebhookconfiguration.admissionregistration.k8s.io/k8s-volume-injector   1          1h
+
+NAME                          TYPE        CLUSTER-IP      EXTERNAL-IP   PORT(S)   AGE
+service/k8s-volume-injector   ClusterIP   10.233.62.149   <none>        443/TCP   1h
+
+NAME                                  READY   UP-TO-DATE   AVAILABLE   AGE
+deployment.apps/k8s-volume-injector   1/1     1            1           1h
+
+# Apply an example (demo1)
+kubectl apply -f examples/demo1.yaml
+```
+```bash
+# Check if the webhook has work (mounts /etc/ssl/certs from node)
+$ kubectl -n k8s-volume-injector-demo1 get po demo1 -o json | jq '.spec.volumes[]|select(.name=="etc-ssl-certs")'
+{
+  "hostPath": {
+    "path": "/etc/ssl/certs/",
+    "type": ""
+  },
+  "name": "etc-ssl-certs"
+}
+```
+
+## How to uninstall
+```bash
+kubectl -n k8s-volume-injector delete \
+  mutatingwebhookconfigurations/k8s-volume-injector \
+  service/k8s-volume-injector \
+  deployment.apps/k8s-volume-injector \
+  issuers.cert-manager.io/k8s-volume-injector \
+  certificates.cert-manager.io/k8s-volume-injector \
+  namespace/k8s-volume-injector
+```
---
+++ deploy/install.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+set -e
+export TMPDIR=$(mktemp -d)
+trap "rm -rf $TMPDIR" EXIT
+
+export VERSION="${VERSION:-main}"
+export INJECTOR_VERSION_TAG="${INJECTOR_VERSION_TAG:-release-v0.5.0}"
+export NAMESPACE="${NAMESPACE:-k8s-volume-injector}"
+
+export REPO_RAW_BASE_URL="https://raw.githubusercontent.com/atorrescogollo/k8s-volume-injector/${VERSION}"
+export INSTALLATION_TYPE="${INSTALLATION_TYPE:-remote}"
+
+
+function main(){
+    local installation_type="$1"
+    case "$installation_type" in
+        remote)
+            downloadTemplates "$TMPDIR"
+            renderAndApply    "$TMPDIR"
+            ;;
+        local)
+            local rootdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." &> /dev/null && pwd )"
+            [ ! -f "$rootdir/manifests/webhook.yaml.template" ] \
+                && echo >&2 "ERR: Templates were not found" \
+                && exit 1
+            renderAndApply "$rootdir"
+            ;;
+    esac
+}
+
+function downloadTemplates(){
+    local rootdir="$1"
+    mkdir -p "$rootdir/manifests/"
+    for i in namespace certificate deployment webhook
+    do
+        wget -O "$rootdir/manifests/$i.yaml" \
+            "$REPO_RAW_BASE_URL/manifests/$i.yaml.template"
+    done
+}
+
+
+function renderAndApply(){
+    local rootdir="$1"
+    mkdir -p "$rootdir/_gen"
+
+    cat "$rootdir/manifests/namespace.yaml.template" \
+        | sed 's@${NAMESPACE}@'$NAMESPACE'@g' \
+        > "$rootdir/_gen/namespace.yaml"
+    cat "$rootdir/manifests/certificate.yaml.template" \
+        | sed 's@${NAMESPACE}@'$NAMESPACE'@g' \
+        > "$rootdir/_gen/certificate.yaml"
+    cat "$rootdir/manifests/deployment.yaml.template" \
+        | sed 's@${NAMESPACE}@'$NAMESPACE'@g' \
+        | sed 's@${INJECTOR_VERSION_TAG}@'$INJECTOR_VERSION_TAG'@g' \
+        > "$rootdir/_gen/deployment.yaml"
+
+    kubectl apply -f "$rootdir/_gen/namespace.yaml"
+    kubectl apply -f "$rootdir/_gen/certificate.yaml"
+    kubectl apply -f "$rootdir/_gen/deployment.yaml"
+
+    local caBundle=$( kubectl get secrets -n $NAMESPACE k8s-volume-injector-cert -o go-template="{{ index .data \"ca.crt\"}}" )
+    cat "$rootdir/manifests/webhook.yaml.template" \
+        | sed 's@${NAMESPACE}@'$NAMESPACE'@g' \
+        | sed 's@${CABUNDLE}@'$caBundle'@g' \
+        > "$rootdir/_gen/webhook.yaml"
+    kubectl apply -f "$rootdir/_gen/webhook.yaml"
+
+}
+
+
+main "${INSTALLATION_TYPE}"
